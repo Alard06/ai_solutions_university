6f0d090caae9e84913c47ee7d06c58e0b5b9525f
alard06 <alard123@yandex.ru>
2025-03-12 17:40:28 +0800

    create class ChatGPT

---
+++ utils/chatgpt_req.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import httpx
+import os
+
+
+class ChatGPT:
+    """
+    Класс для работы с ChatGPT API.
+    """
+
+    def __init__(self):
+        load_dotenv()
+        
+        proxy_url = "http://QCbDT52oW8QR:RNW78Fm5@pool.proxy.market:10344"
+
+        self.http_client = httpx.Client(proxy=proxy_url)
+        self.client = OpenAI(
+            api_key=os.getenv("API_KEY_CHATGPT"),
+            http_client=self.http_client
+        )
+
+    def get_answer(self, question: str) -> str:
+        """
+        Получает ответ на тестовый вопрос.
+        :param question: str - вопрос
+        :return: str - ответ
+        """
+        completion = self.client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {"role": "developer", "content": "Ты должен выбирать правильный вариант ответа из предложенных вариантов. Отвечай только одним вариантом, без пояснений."},
+            {"role": "user", "content": question}
+        ]
+        )
+        return completion.choices[0].message.content
+
+
+chatgpt = ChatGPT()
+print(chatgpt.get_answer("""Что является основным преимуществом распределённой архитектуры?
+Все компоненты системы запускаются на одном сервере
+Система не требует сложных механизмов для взаимодействия
+Обновление компонентов системы требует полной остановки всех сервисов
+Система может быть масштабирована по мере необходимости
+Весь трафик проходит через один сервер"""))
